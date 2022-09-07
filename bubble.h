6cad66988dc36472e7faed116005cf0274e7c341
JP-coder2000 <70972830+JP-coder2000@users.noreply.github.com>
2022-09-06 20:18:27 -0500

    Add files via upload

---
+++ bubble.h
@@ -0,0 +1,57 @@
+// =================================================================
+//
+// File: bubble.h
+// Author: Pedro Perez
+// Description: This file contains the implementation of the
+//				bubble sort.
+//
+// Copyright (c) 2020 by Tecnologico de Monterrey.
+// All Rights Reserved. May be reproduced for any non-commercial
+// purpose.
+// =================================================================
+#ifndef BUBBLE_H
+#define BUBBLE_H
+
+#include "header.h"
+#include <vector>
+
+// =================================================================
+// Performs the bubble sort algorithm on an array
+//
+// @param A, an array of T elements.
+// @param size, the number of elements in the array.
+// =================================================================
+template <class T>
+int bubbleSort(T *arr, int size) {
+	int cont = 0;
+	for(int i = size - 1; i > 0; i--){
+		for(int j = 0; j < i; j++){
+			if(arr[j] > arr[j + 1]){
+				swap(arr, j, j + 1);
+				cont++;
+			}
+		}
+	}
+	return cont;
+}
+
+// =================================================================
+// Performs the bubble sort algorithm on an vector.
+//
+// @param v, a vector of T elements.
+// =================================================================
+template <class T>
+int bubbleSort(std::vector<T> &v) {
+	int cont = 0;
+	for(int i = v.size() - 1; i > 0; i--){
+		for(int j = 0; j < i; j++){
+			if(v[j] > v[j + 1]){
+				swap(v, j, j + 1);
+				cont++;
+			}
+		}
+	}
+	return cont;
+}
+
+#endif /* BUBBLE_H */
